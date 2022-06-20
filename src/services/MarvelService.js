31083d6511769785ca9f047597274d1401b28f7d
Artyom <art161994@yandex.ru>
2022-06-20 11:42:01 +0300

    deploying on heroku

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,7 +1,7 @@
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     
-    _apiKey = 'apikey=c5d6fc8b83116d92ed468ce36bac6c62';
+    _apiKey = 'apikey=0f8b5ae5818bbdea4768c43265c0ac92';
     _baseOffset = 210;
 
     getResource = async (url) => {
