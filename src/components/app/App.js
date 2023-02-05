a07e75a1f8c7e13632203ae5095c0c5e52a05dce
Artyom <art161994@yandex.ru>
2023-02-05 15:26:20 +0100

    work with hooks

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,4 +1,4 @@
-import { Component } from "react/cjs/react.development";
+import { useState } from "react/cjs/react.development";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
@@ -7,20 +7,17 @@ import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 import decoration from '../../resources/img/vision.png';
 
-class App extends Component {
+const App = () => {
+
+    const [selectedChar, setChar] = useState(null);
     
-    state = {
-        selectedChar: null 
+
+    const onCharSelected = (id) => {
+        setChar(id);
     }
 
-    onCharSelected = (id) => {
-        this.setState({
-            selectedChar: id
-        })
-    }
 
-    render () {
-            return (
+        return (
             <div className="app">
                 <AppHeader/>
                 <main>
@@ -29,17 +26,16 @@ class App extends Component {
                     </ErrorBoundary>
                     <div className="char__content">
                         <ErrorBoundary>
-                            <CharList onCharSelected={this.onCharSelected}/>
+                            <CharList onCharSelected={onCharSelected}/>
                         </ErrorBoundary>
                         <ErrorBoundary>
-                            <CharInfo charId={this.state.selectedChar}/>
+                            <CharInfo charId={selectedChar}/>
                         </ErrorBoundary>
                     </div>
                     <img className="bg-decoration" src={decoration} alt="vision"/>
                 </main>
             </div>
         )
-    }
 }
 
 export default App;
